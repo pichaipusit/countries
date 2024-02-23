1e13b476a722d8a3c9b73e2c349f6f97f2bd1af3
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-23 09:36:16 +0700

    Setup project

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header class="w-full bg-red-400">
+    <h1>Countries of the World</h1>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped></style>
